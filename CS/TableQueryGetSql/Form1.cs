d228ae5ecd91a211787871a9c4bcad93c6c47dcf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/TableQueryGetSql/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraReports.UI;
+
+namespace TableQueryGetSql {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            XtraReport1 report = new XtraReport1();
+            ReportPrintTool pt = new ReportPrintTool(report);
+            pt.ShowPreviewDialog();
+        }
+    }
+}
